9eaa13e7bc6d46ea21f235ffcbab3475670e33c9
suhkadam <45067512+suhkadam@users.noreply.github.com>
2018-11-19 21:53:31 +0530

    Updated sel.java
    updated sel.java

--- src/list/sel.java
+++ src/list/sel.java
@@ -10,6 +10,9 @@ public class sel {
         WebDriver driver= new ChromeDriver();
         driver.get("https://www.google.com/");
         System.out.println(driver.getTitle());
+        driver.get("http://automationpractice.com/index.php");
+		driver.findElement(By.name("search_query")).sendKeys("Printed Chiffon Dress");
+		driver.findElement(By.name("submit_search")).sendKeys(Keys.ENTER);
         driver.close();
     }
 
